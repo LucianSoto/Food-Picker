46a9b597ce0055af08e01ab885b78f236cedf88c
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-09-02 18:10:19 -0700

    login error handling

--- pages/login/loginStyles.js
+++ pages/login/loginStyles.js
@@ -1,3 +1,4 @@
+import { Certificate } from 'crypto'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 export default EStyleSheet.create({
@@ -76,8 +77,13 @@ export default EStyleSheet.create({
   },
 
   err: {
-    color: 'red'
+    color: 'red',
+    fontSize: 30,
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    marginTop: 30,
   },
+
   errorText: {
     color: 'red'
   }
