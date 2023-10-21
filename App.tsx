eb1f612df1607caab084341ae5af998e02a85d1f
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-10-21 01:10:01 -0700

    Corrected statusbar color for ios

--- App.tsx
+++ App.tsx
@@ -4,8 +4,13 @@
 import React, { useEffect } from 'react'
 import {
   StatusBar,
+  View,
+  SafeAreaView,
   useColorScheme,
+  StyleSheet,
 } from 'react-native';
+// Do I need to import platform for react-native?? seems to be working fine w/o it.
+import { useSafeAreaInsets, SafeAreaProvider} from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SplashScreen from 'react-native-splash-screen'
@@ -21,26 +26,35 @@ function App(): JSX.Element {
   
   useEffect(()=> {
     // do stuff while splash screen is shown After having done stuff (such as async tasks) hide the splash screen
-    console.log('closing splash APP TSX',)
     setTimeout(() => {
       SplashScreen.hide();
     }, 3000);
-
   },[])
+  
+  const CustomStatusBar = ({backgroundColor}) => { 
+    const insets = useSafeAreaInsets();
+    return (
+      <View style={{ height: insets.top, backgroundColor }}>
+        <StatusBar
+          animated={true}
+          backgroundColor={backgroundColor}
+        />
+      </View>
+    );
+  }
 
   return (  
-    <NavigationContainer>
-        <StatusBar
-          barStyle={'light-content'}
-          backgroundColor={EStyleSheet.value('$mainColor_magenta')}
-        />
-      <Stack.Navigator screenOptions={{headerShown: false}} >
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="Forgot_Password" component={ForgotPassword} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <CustomStatusBar backgroundColor={EStyleSheet.value('$mainColor_magenta')} />
+        <Stack.Navigator screenOptions={{headerShown: false}} >
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Home" component={Tabs} />
+          <Stack.Screen name="Forgot_Password" component={ForgotPassword} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
@@ -51,3 +65,23 @@ EStyleSheet.build({
   $mainColor_black: '#050102',
   $mainColor_magenta: '#c2003f',
 })
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor:'#79B45D',
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#33373B',
+  },
+});
