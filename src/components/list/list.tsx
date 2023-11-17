a1360be04f3683c09c21cd5d1e03e7b8ff9b3e0b
Lucian Brooks <74628091+LucianAiolos@users.noreply.github.com>
2023-11-17 15:00:49 -0800

    updated firebase auth/app to match firebase firestore version

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,14 +1,10 @@
-import { Text, View, Image, ImageStyle } from 'react-native'
+import { Text, View, Image, ImageStyle, TouchableOpacity } from 'react-native'
 import styled from 'styled-components/native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import styles from './listStyles'
-import {Dimensions} from 'react-native'
+import firestore from '@react-native-firebase/firestore';
 
 Icon.loadFont().catch((error) => { console.info(error); });
-
-const winWidth = Dimensions.get('window').width
-console.log(winWidth, 'winWidth')
-console.log(Dimensions.get('window').height, 'height')
 
 type Data = {
   name: string, 
@@ -21,17 +17,19 @@ type Data = {
   categories: [{title: string}],
 }
 
-const Item = styled.View`
-  margin: 10px;
-  padding: 10px;
-  background-color: white;
-  font-size: 15px;
-  flexDirection: row;
-  border-radius: 5px;
-`
-
-
 const List = (data: any) => { 
+  const addToFavs = async (name: string) => {
+    firestore()
+      .collection('User_Favorites')
+      .doc('ABC')
+      .set({
+        name: name,
+      })
+      .then(() => {
+        console.log('User added!');
+      });
+  }
+  
   const list = data.data.map((item: Data, i: number)=> {
     let getCategories = item.categories.map((category: {title: string}, i:number)=> {
       return (
@@ -40,13 +38,13 @@ const List = (data: any) => {
         </Text>
       )
     }) 
-
+    
     return (
       <Item key={i}>
         <Image 
           style={styles.thumb as ImageStyle} // had to add type for typescript compiling.
           source={{uri: item.image_url}}
-        />
+          />
         <View style={styles.right_container}>
           <Text style={styles.name}>{item.name}</Text>
           <Text>
@@ -55,7 +53,7 @@ const List = (data: any) => {
               name="phone"
               color="black"
               size={15}
-            />
+              />
             {/* deal with removing +1 on display */}
             {item.phone.replace(/^[\s\S]{0,2}/g, "  ")} 
           </Text>
@@ -64,16 +62,23 @@ const List = (data: any) => {
               name="star"
               color="red"
               size={15}
-            /> 
+              /> 
             <Text>  {item.rating}</Text>
           </View>
           <Text style={styles.price}>{item.price}</Text>
           <View style={styles.categories_container}>{getCategories}</View>
         </View>
+        <TouchableOpacity onPress={()=> addToFavs(item.name)}>
+          <Icon 
+            name="heart-o"
+            color="red"
+            size={20}
+            />
+        </TouchableOpacity>
       </Item>
     )
   })
-
+  
   return (
     <View>
       {list}
@@ -83,3 +88,12 @@ const List = (data: any) => {
 
 export default List
 
+
+const Item = styled.View`
+  margin: 10px;
+  padding: 10px;
+  background-color: white;
+  font-size: 15px;
+  flexDirection: row;
+  border-radius: 5px;
+`
